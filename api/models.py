d7a50f095c201fa3745ecd030ed5557ceb58b162
GabrielB64 <gabriel.bustamante04@inacap.cl>
2025-08-25 09:39:20 -0400

    25-8

--- api/models.py
+++ api/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class programmer(models.Model):
-    fullname=models.CharField(max_length=100)
-    nickname=models.CharField(max_length=100)
-    language=models.CharField(max_length=100)
-    age=models.PositiveSmallIntegerField()
-    is_active=models.BooleanField(default=True)
+class Proyecto(models.Model):
+    proyecto_name = models.CharField(
+        max_length=100, verbose_name="Nombre del Proyecto"
+    )
+    descripcion = models.TextField(verbose_name="Descripción")
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Usuario"
+    )
+    def __str__(self):
+        return self.proyecto_name
+    
+class Tarea(models.Model):
+    task_name = models.CharField(
+        max_length=100, verbose_name="Nombre de la Tarea"
+    )
+    completada = models.BooleanField(
+        default=False, verbose_name="¿Completada?"
+    )
+    proyecto = models.ForeignKey(
+        Proyecto, on_delete=models.CASCADE
+    )
